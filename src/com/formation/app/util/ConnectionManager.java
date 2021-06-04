a53766f35a1a12aa622443d7d4c47b4fee99b07f
DSARRAZY <d.sarrazy@gmail.com>
2021-06-04 13:03:30 +0200

    paramètre base de données

--- src/com/formation/app/util/ConnectionManager.java
+++ src/com/formation/app/util/ConnectionManager.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class ConnectionManager {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/bdd_java";
+    private static final String URL = "jdbc:mysql://localhost:3306/paradise";
     private static final String USER = "root";
     private static final String PASSWORD = "Damien81!";
 
