353ef95f9da6022d662a6c93dec75fc0bcf44a50
DSARRAZY <d.sarrazy@gmail.com>
2021-06-04 10:52:36 +0200

    class ConnectionManager

---
+++ src/com/formation/app/util/ConnectionManager.java
@@ -0,0 +1,51 @@
+package com.formation.app.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionManager {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/bdd_java";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Damien81!";
+
+    private static Connection connection;
+
+    private ConnectionManager(){
+
+    }
+
+    public static Connection getConnection() {
+        if(connection == null) {
+            try {
+                loadDriver();
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+                connection.setAutoCommit(false);
+            } catch (SQLException e) {
+                throw new RuntimeException("Cannot open connection");
+            }
+        }
+        return connection;
+    }
+
+    public static void loadDriver(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.err.println("Driver MySQL introuvable");
+        }
+    }
+
+    public static void closeConnection(){
+        try {
+            connection.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+    }
+
+}
+
